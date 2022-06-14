8eb09c68147eb157f3ad15e14bb6f14b1a844f5e
Polina-Oleshkevich <94075525+Polina-Oleshkevich@users.noreply.github.com>
2022-06-14 19:00:31 +0300

    Add files via upload

---
+++ TestCommand.py
@@ -0,0 +1,11 @@
+import math
+help(math)
+print(dir(math))
+help(math.sqrt)
+list1 = [82,8,23,97,92]
+dir(list1)
+help(list1.insert)
+tuple = [82,8,23,97,92]
+dir(tuple)
+help(tuple.insert)
+
---
+++ test.py
@@ -0,0 +1,8 @@
+a=int(input())
+print(a**2)
+input()
+import time
+delay=3
+a=a=int(input())
+print(a**2)
+time.sleep(delay)
